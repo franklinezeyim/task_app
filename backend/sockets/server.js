a20e6d56022187655f74398cf38358f1cf82a574
Franklin Ezeyim <ezeyimf@gmail.com>
2026-02-11 22:44:08 +0100

    socket and cors issue fixed

--- backend/sockets/server.js
+++ backend/sockets/server.js
@@ -3,9 +3,14 @@ import { Server } from "socket.io";
 import { setupSocket } from "./socket.js"; // Your existing handler
 
 export const initSocket = (server) => {
+  const allowedOrigins = [
+  'http://localhost:5173',
+  'https://taskflow-xi-seven.vercel.app',
+];
+
   const io = new Server(server, {
     cors: {
-      origin: "https://taskflow-xi-seven.vercel.app/", // Your React app port
+      origin: allowedOrigins,
       methods: ["GET", "POST"],
       credentials: true
     }
